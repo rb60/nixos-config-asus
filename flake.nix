08d78d3926e281e80d0db2b3b40ca3c5f2c30449
rb60 <rmngncua@gmail.com>
2026-01-15 02:38:05 +0200

    nixos updated to 25.11

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
 	description = "nixos config";
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 		home-manager = {
-			url = "github:nix-community/home-manager/release-25.05";
+			url = "github:nix-community/home-manager/release-25.11";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 
 		nixvim = {
-			url = "github:nix-community/nixvim/nixos-25.05";
+			url = "github:nix-community/nixvim/nixos-25.11";
 			inputs.nixpkgs.follows = "nixpkgs";
 
 		};
