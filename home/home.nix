cf5c2a17e05644ffb1b4a46b4c2a8d9354ffbe74
rb60 <rmngncua@gmail.com>
2026-04-08 23:16:59 +0300

    bug fixes

--- home/home.nix
+++ home/home.nix
@@ -62,7 +62,7 @@
 		"org/gnome/settings-daemon/plugins/media-keys" = {
 			custom-keybindings = [
 				"/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-				"/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1"
+				"/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
 		    	];
 		};
 		"org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
