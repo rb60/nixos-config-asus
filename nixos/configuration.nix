a68a792080f9ac70827f9e40389c0c6c3ad540e4
rb60 <rmngncua@gmail.com>
2026-05-25 23:13:18 +0300

    wayland disabled

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -67,7 +67,7 @@
   ];
 
   services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.wayland.enable = false;
   services.desktopManager.plasma6.enable = true;
 
   services.xserver.xkb = {
