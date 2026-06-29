ce33bcbfe596a27849006cf709d992197a086901
rb60 <rmngncua@gmail.com>
2026-06-29 17:08:03 +0300

    nixvim cmp changed

--- home/modules/nixvim/plugins/cmp.nix
+++ home/modules/nixvim/plugins/cmp.nix
@@ -2,18 +2,19 @@
 	programs.nixvim.plugins.cmp = 
 	{
 		enable = true;
+		autoEnableSources = true;
 		settings = {
 			sources = [
-				{ name = "nvim_lsp";}
+				{ name = "nvim_lsp"; }  
+        			{ name = "luasnip"; }   
+        			{ name = "path"; }      
+        			{ name = "buffer"; }    
 			];
 			completion =
 			{
-				autocomplete = 
-					[
-						"require('cmp.types').cmp.TriggerEvent.TextChanged"
-					];
 				completeopt = "menu,menuone,noselect";
 			};		
 		};
 	};
+	programs.nixvim.plugins.luasnip.enable = true;
 }
