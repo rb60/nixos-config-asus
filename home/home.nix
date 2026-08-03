1c3d7318a3be211e7aa0674e8ca508e72d7f31f7
rb60 <rmngncua@gmail.com>
2026-08-04 01:09:51 +0300

    xmpp bot service added

--- home/home.nix
+++ home/home.nix
@@ -31,21 +31,46 @@
 		enableBashIntegration = true;
 	};
 
-	systemd.user.services.qbittorrent = {
-  		Unit = {
-    			Description = "qBittorrent user service";
-    			After = [ "graphical-session.target" ];
-			RequiresMountsFor = [ "/mnt/Elements" ];
-  		};
-  		Install = {
-    			WantedBy = [ "graphical-session.target" ];
-  		};
-  		Service = {
-    			ExecStart = "${pkgs.qbittorrent}/bin/qbittorrent";
-    			Restart = "on-failure";
-			TimeoutStopSec = "15s";
-  		};
-};
+	systemd.user.services = { 
+		qbittorrent = {
+  			Unit = {
+    				Description = "qBittorrent user service";
+    				After = [ "graphical-session.target" ];
+				RequiresMountsFor = [ "/mnt/Elements" ];
+  			};
+  			Install = {
+    				WantedBy = [ "graphical-session.target" ];
+  			};
+  			Service = {
+    				ExecStart = "${pkgs.qbittorrent}/bin/qbittorrent";
+    				Restart = "on-failure";
+				TimeoutStopSec = "15s";
+  			};
+		};
+		xmppBot = 
+		let
+			xmppPythonEnv = pkgs.python3.withPackages ( python-pkgs : with pkgs.python3Packages; [
+      				yt-dlp
+				pydantic
+				aiohttp
+				slixmpp
+      				]);	
+		in{
+  			Unit = {
+    				Description = "xmppBot user service";
+    				After = [ "graphical-session.target" ];
+				RequiresMountsFor = [ "/mnt/Elements" ];
+  			};
+  			Install = {
+    				WantedBy = [ "graphical-session.target" ];
+  			};
+  			Service = {
+    				ExecStart = "${xmppPythonEnv}/bin/python /home/tima/source/python/audioDownloadBot/main.py";
+    				Restart = "on-failure";
+  			};
+		};
+	};
+
 
 
 	
